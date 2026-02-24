82a60b144b9970198e09693ca5e39321292ad100
Fayyaz Shah <fayyazhs107@gmail.com>
2026-02-24 22:52:35 +0100

    Update views.py

--- orders/views.py
+++ orders/views.py
@@ -8,6 +8,7 @@ from .models import Order
 from .serializers import OrderSerializer
 from .services import place_order
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from restaurants.models import Branch
 
 
 # List all menu categories with items
@@ -20,20 +21,25 @@ class MenuListView(generics.ListAPIView):
 # Create order endpoint
 class PlaceOrderView(APIView):
     permission_classes = [IsAuthenticated]
+
     def post(self, request, *args, **kwargs):
         user = request.user
         branch_id = request.data.get("branch_id")
+        branch = Branch.objects.get(id=branch_id, is_active=True)
         items_data = request.data.get("items")  # list of {"menu_item_id": X, "quantity": Y}
 
         if not branch_id or not items_data:
             return Response({"error": "branch_id and items are required"}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            order = place_order(user, branch_id, items_data)
+            order = place_order(user, branch, items_data)
             serializer = OrderSerializer(order)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Order could not be processed"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 # List user orders
@@ -42,4 +48,9 @@ class UserOrdersView(generics.ListAPIView):
     serializer_class = OrderSerializer
 
     def get_queryset(self):
-        return Order.objects.filter(user=self.request.user).prefetch_related("items__menu_item")
+        return (
+            Order.objects
+            .filter(user=self.request.user)
+            .select_related("branch")
+            .prefetch_related("items__menu_item")
+        )
